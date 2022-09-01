67bd58261d97a449f0c28e237fdb2ed4ba4f1e33
Dylan Bruner <brunerdylan5@gmail.com>
2022-09-01 15:13:57 -0400

    Update Password & Cloud

--- passwordprotect.js
+++ passwordprotect.js
@@ -1,9 +1,10 @@
 console.log("passwordprotect.js");
 
 if (localStorage['loggedIn'] == undefined){localStorage['loggedIn'] = 'false'}
+localStorage['loggedIn'] = 'false'
 
 if (localStorage['loggedIn'] == 'false'){
-    if (prompt("Password?") !== "12345"){console.log("wrong password"); window.location = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'}
+    if (prompt("Password?") !== "1915"){console.log("wrong password"); window.location = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'}
     else {localStorage['loggedIn'] = 'true'; console.log("correct password");}
 }
 else {console.log("already logged in");}
